9e1f63078332e52676121b42b2da941ad7253905
Christian <elchristog@gmail.com>
2023-07-26 11:18:13 -0400

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -5,7 +5,8 @@ import datetime
 import streamlit as st
 
 # Database functions
-def create_tables(conn):
+def create_tables():
+    conn = sqlite3.connect('marketing_funnels.db')
     cur = conn.cursor()
 
     cur.execute("""
@@ -37,15 +38,18 @@ def create_tables(conn):
     """)
 
     conn.commit()
+    conn.close()
 
 @st.cache(allow_output_mutation=True)
-def get_data(conn, query):
-    return pd.read_sql_query(query, conn)
+def get_data(query):
+    conn = sqlite3.connect('marketing_funnels.db')
+    df = pd.read_sql_query(query, conn)
+    conn.close()
+    return df
 
 # Streamlit app
 def main():
-    conn = sqlite3.connect('marketing_funnels.db')
-    create_tables(conn)
+    create_tables()
 
     st.title("Marketing Funnels")
 
@@ -61,23 +65,27 @@ def main():
         name = st.text_input("Enter the name of the funnel step")
         order_number = st.number_input("Enter the order number of the funnel step", value=1, step=1)
         if st.button("Add Funnel Step"):
+            conn = sqlite3.connect('marketing_funnels.db')
             cur = conn.cursor()
             cur.execute("INSERT INTO funnel_steps (name, order_number) VALUES (?, ?)", (name, order_number))
             conn.commit()
+            conn.close()
             st.success("Funnel step added successfully.")
 
     elif choice == "Add registration":
         st.subheader("Add a new registration")
         # Fetch funnel steps
-        df_funnel_steps = get_data(conn, "SELECT * FROM funnel_steps")
+        df_funnel_steps = get_data("SELECT * FROM funnel_steps")
         funnel_step_id = st.selectbox("Select funnel step", df_funnel_steps['id'])
         description = st.text_input("Enter a description for the registration")
         realizations = st.number_input("Enter the number of realizations", value=1, step=1)
         if st.button("Add Registration"):
             date = datetime.datetime.now().strftime("%Y-%m-%d")
+            conn = sqlite3.connect('marketing_funnels.db')
             cur = conn.cursor()
             cur.execute("INSERT INTO registrations (funnel_step_id, description, realizations, date) VALUES (?, ?, ?, ?)", (funnel_step_id, description, realizations, date))
             conn.commit()
+            conn.close()
             st.success("Registration added successfully.")
 
     elif choice == "Add hypothesis":
@@ -86,9 +94,11 @@ def main():
         description = st.text_input("Enter a description for the hypothesis")
         if st.button("Add Hypothesis"):
             date = datetime.datetime.now().strftime("%Y-%m-%d")
+            conn = sqlite3.connect('marketing_funnels.db')
             cur = conn.cursor()
             cur.execute("INSERT INTO hypotheses (name, description, date) VALUES (?, ?, ?)", (name, description, date))
             conn.commit()
+            conn.close()
             st.success("Hypothesis added successfully.")
 
     elif choice == "View funnel":
@@ -96,7 +106,7 @@ def main():
         start_date = st.date_input("Enter the start date")
         end_date = st.date_input("Enter the end date")
         if st.button("View Funnel"):
-            df = get_data(conn, f"""
+            df = get_data(f"""
             SELECT
                 funnel_steps.id,
                 funnel_steps.name,
@@ -114,7 +124,7 @@ def main():
         start_date = st.date_input("Enter the start date")
         end_date = st.date_input("Enter the end date")
         if st.button("View Funnel by Week"):
-            df = get_data(conn, f"""
+            df = get_data(f"""
             SELECT
                 date,
                 funnel_steps.name,
